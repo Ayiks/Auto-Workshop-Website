3ac1c673094415ff5fb7f9dffdef53d7d05759bb
Ayiks <cliffysarp@gmail.com>
2026-01-14 22:57:13 +0000

    component export file

---
+++ frontend/src/components/common/index.js
@@ -0,0 +1,6 @@
+// Common components barrel export
+export { default as Button } from './Button';
+export { default as Card, StatCard } from './Card';
+export { default as LoadingSpinner, LoadingOverlay } from './LoadingSpinner';
+export { default as EmptyState } from './EmptyState';
+export { default as ProtectedRoute } from './ProtectedRoute';

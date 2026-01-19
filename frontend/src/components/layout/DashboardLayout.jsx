47f90f3ea49b4f7b612563c3a84e73adc2813735
Ayiks <cliffysarp@gmail.com>
2026-01-19 23:43:13 +0000

    icon change

--- frontend/src/components/layout/DashboardLayout.jsx
+++ frontend/src/components/layout/DashboardLayout.jsx
@@ -101,16 +101,22 @@ export default function DashboardLayout() {
       path: "/expenses",
       icon: (
         <svg
-          className="w-5 h-5"
+          xmlns="http://www.w3.org/2000/svg"
+          class="w-6 h-6"
           fill="none"
-          stroke="currentColor"
           viewBox="0 0 24 24"
+          stroke="currentColor"
+          stroke-width="1.5"
         >
+          {/* <path 
+    stroke-linecap="round" 
+    stroke-linejoin="round" 
+    d="M12 6v12m-3-2.818.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33" 
+  /> */}
           <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={1.5}
-            d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M2.25 6L9 12.75l4.286-4.286L21 16.5m0 0h-5.25m5.25 0V11.25"
           />
         </svg>
       ),
@@ -125,13 +131,12 @@ export default function DashboardLayout() {
           fill="none"
           stroke="currentColor"
           viewBox="0 0 24 24"
-          xmlns="http://www.w3.org"
         >
           <path
             strokeLinecap="round"
             strokeLinejoin="round"
             strokeWidth={1.5}
-            d="M19 14l-7 7m0 0l-7-7m7 7V3"
+            d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
           />
         </svg>
       ),
